01803891a4a9c2da8cc41e9f2f094872a8c76f73
gajananchoundhe <gajanan.choundhe@gmail.com>
2020-01-20 21:59:29 +0530

    Add files via upload

---
+++ ClientNode.bat
@@ -0,0 +1,16 @@
+@echo off 
+
+TITLE Client Node
+
+REM This is parameter value reading
+
+set Host=%1
+set dir=%2
+echo Parameter host: %HOST% 
+echo Paramter direcoty path: %dir%
+
+Echo.
+Echo Connecting to grid hub sever, please wait...
+Echo.
+
+java -jar %dir%\selenium-server-standalone-3.141.59.jar -role node -hub http://%Host%:4444/grid/register -port 5555
